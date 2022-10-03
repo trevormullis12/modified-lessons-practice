2d6e4d0da87e7510583f1e4afacb37b186227020
Trevor Mullis <trevormullis@students-MacBook-Pro.local>
2022-10-03 10:05:44 -0500

    initial commit

---
+++ src/main/java/com/codeup/modifiedlessonspractice/ModifiedlessonspracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.modifiedlessonspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModifiedlessonspracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModifiedlessonspracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/modifiedlessonspractice/ModifiedlessonspracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.modifiedlessonspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModifiedlessonspracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
