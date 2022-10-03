251928dda9776975a134a521e55dc2e52976ce3b
Trevor Mullis <trevormullis@students-MacBook-Pro.local>
2022-10-03 15:30:57 -0500

    added jalopy

---
+++ src/main/java/com/codeup/modifiedlessonspractice/models/Post.java
@@ -0,0 +1,17 @@
+package com.codeup.modifiedlessonspractice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+
+}
---
+++ src/main/java/com/codeup/modifiedlessonspractice/models/User.java
@@ -0,0 +1,2 @@
+package com.codeup.modifiedlessonspractice.models;public class User {
+}
